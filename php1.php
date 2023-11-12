95e7ad61fda5c7590de03c7ef5d3ed6279dd467c
azmarifdev <azmarif.dev@gmail.com>
2023-11-12 08:37:15 +0600

    some

---
+++ php1.php
@@ -0,0 +1,12 @@
+<?php
+// Variable
+$country = 'Bangladesh';
+$age = 45;
+$my_name = 'arif';
+
+$word = "age";
+
+echo "Hello Bangladesh";
+echo "\n";
+echo "My age is {$age} and my name is {$my_name}\n";
+echo "I live in {$country}";
